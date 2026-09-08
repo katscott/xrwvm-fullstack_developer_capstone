e2cebf27e58d2a0679ff2ca4e3889f17aff5b5fc
Katie Scott <gh@aka.katscott.me>
2026-09-08 13:49:59 -0700

    task 8

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-APP_URL = 'https://coursera1429-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+# APP_URL = 'https://coursera1429-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+APP_URL = 'https://coursera1429-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 
 ALLOWED_HOSTS=['localhost',APP_URL]
 CSRF_TRUSTED_ORIGINS=[APP_URL]
